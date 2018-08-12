fef8348d25fbe49bde3683a1b96eab12311b12ea
Alfie Darko <me@alfiedarko.co.uk>
2018-08-12 18:46:52 +0100

    abstracted logic away from controller into its own method

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import {
   tournamentSchema,
   Tournament
 } from '../models/tournament'
+import tournamentService from '../services/tournamentService'
 const router = Router();
 
 /* GET home page. */
@@ -15,6 +16,8 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/api/v1/tournaments', function (req, res, next) {
+
+
   res.json(
     [{
       'tournamentName': 'Makers PingPong',
@@ -33,34 +36,17 @@ router.get('/api/v1/tournaments', function (req, res, next) {
 });
 
 router.post('/api/v1/tournaments', async function (req, res, next) {
-
-  let newTournament = new Tournament({
-    tournamentName: req.body.tournamentName,
-    players: {
-      playerA: req.body.players.playerA,
-      playerB: req.body.players.playerB,
-      playerC: req.body.players.playerC,
-      playerD: req.body.players.playerD,
-      playerE: req.body.players.playerE,
-      playerF: req.body.players.playerF,
-      playerG: req.body.players.playerG,
-      playerH: req.body.players.playerH,
-
-    }
-  })
-
   try {
-    const savedTournament = await newTournament.save()
+    const savedTournament = await tournamentService.create(req)
     return res.status(201).json({
       success: savedTournament,
       message: 'Success!'
     }).end();
   } catch (error) {
-    return res.status(500).json({
-      error: 'Missing parameters'
+    return res.status(400).json({
+      error: 'Bad Request: Make sure you have filled in all parameters'
     }).end()
   }
-
 })
 
 export default router;
