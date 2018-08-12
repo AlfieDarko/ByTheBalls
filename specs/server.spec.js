fe3545fbb9223e0146f97cf27353155a064d9d34
Alfie Darko <me@alfiedarko.co.uk>
2018-08-12 09:57:55 +0100

    REFACTOR: server.spec.js > neaten up the testing suite.

--- specs/server.spec.js
+++ specs/server.spec.js
@@ -5,16 +5,19 @@ test('Server Testing Suite loads', () => {
     expect(1).toBe(1)
 });
 
-describe('Test the root path', () => {
-    it('It should response the GET method', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
+test('GET Paths', () => {
+    describe('Test the root path', () => {
+        it('It should response the GET method', async () => {
+            const response = await request(app).get('/');
+            expect(response.statusCode).toBe(200);
+        });
+    })
+    
+    describe('GET /api/v1/tournaments', () => {
+        it('Can get all tournaments', async () => {
+            const response = await request(app).get('/api/v1/tournaments')
+            expect(response.body).toBeInstanceOf(Object);
+        });
     });
-})
-
-describe('GET /api/v1/tournaments', () => {
-    it('Can get all tournaments', async () => {
-        const response = await request(app).get('/api/v1/tournaments')
-        expect(response.body).toBeInstanceOf(Object);
-    });
+    
 });
