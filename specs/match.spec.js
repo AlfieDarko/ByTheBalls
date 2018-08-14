bcf35661ebf34d747397f29925bc212325a92415
Alfie Darko <mrhazzy@gmail.com>
2018-08-14 12:03:32 +0100

    GREEN: Match > When instantiated with 2 player classes > .setFirstMatchWinner() > Should NOT call the .winFirstMatch() on the player 2 class instance

--- specs/match.spec.js
+++ specs/match.spec.js
@@ -17,9 +17,14 @@ describe('Match', () => {
   let match = new Match(player1, player2);
   describe('When instantiated with 2 player classes', () => {
     describe('.setFirstMatchWinner()', () => {
-      it('Should call the .winFirstMatch() on selected Player class', () => {
+      it('Should call the .winFirstMatch() on selected player1 class instance', () => {
         match.setFirstMatchWinner(player1);
         expect(player1.winFirstMatch).toHaveBeenCalledTimes(1);
+      });
+
+      it('Should NOT call the .winFirstMatch() on the player2 class instance', () => {
+        match.setFirstMatchWinner(player1);
+        expect(player2.winFirstMatch).not.toHaveBeenCalledTimes(1);
       });
     });
   });
