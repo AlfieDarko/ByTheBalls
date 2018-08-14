b2bb90225a7a5dcd4e8613e5ec9e252bd2f0dacc
Alfie Darko <mrhazzy@gmail.com>
2018-08-14 23:22:21 +0100

    GREEN: Tournament > .getName() > should return the correct name property

--- specs/tournament.spec.js
+++ specs/tournament.spec.js
@@ -1,8 +1,11 @@
 import Tournament from '../src/Tournament';
 
-describe('Placeholder', () => {
-  it('', () => {
-    expect(1).toBe(1);
-
+describe('Tournament', () => {
+  describe('.getName()', () => {
+    it('should return the correct name property', () => {
+      const TOURNAMENT_NAME = 'MakersPingPong';
+      let tournament = new Tournament(TOURNAMENT_NAME);
+      expect(tournament.getName()).toBe(TOURNAMENT_NAME);
+    });
   });
 });
--- src/Tournament.js
+++ src/Tournament.js
@@ -1,1 +1,9 @@
-export default class Tournament {}
+export default class Tournament {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
