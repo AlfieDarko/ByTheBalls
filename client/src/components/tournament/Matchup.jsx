31b07b6c853a9aaaf4bdf54864fb41443f202a6a
Alfie Darko <mrhazzy@gmail.com>
2018-08-21 13:28:28 +0100

    Added Matchup Component

---
+++ client/src/components/tournament/Matchup.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import styles from '../../styles/main.less';
+
+const Matchup = ({ playerA, playerB }) => (
+  <Fragment>
+    <ul className={styles.matchup}>
+      <li className={`${styles.team} ${styles['team-top']}`}>
+        {playerA}
+        <span className={styles.score}>score 00</span>
+      </li>
+      <li className={`${styles.team} ${styles['team-bottom']}`}>
+        {playerB}
+        <span className={styles.score}>score 00</span>
+      </li>
+    </ul>
+  </Fragment>
+);
+
+Matchup.propTypes = {
+  playerA: PropTypes.string.isRequired,
+  playerB: PropTypes.string.isRequired,
+};
+
+export default Matchup;
