2de55e1668690908fa1034cc3a143c496063d20b
Alfie Darko <mrhazzy@gmail.com>
2018-08-21 13:30:58 +0100

    Made changes to webpack to parse font-awesome styles

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const STYLESHEETS_DIR = path.resolve(__dirname, './src/styles/');
@@ -7,46 +7,61 @@ const STYLESHEETS_DIR = path.resolve(__dirname, './src/styles/');
 module.exports = {
   entry: './src/index.js',
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }, {
-      test: /\.less$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader",
-          options: {
-            sourceMap: true,
-            modules: true,
-            localIdentName: "[local]___[hash:base64:5]"
-          }
-        },
-        {
-          loader: "less-loader"
-        }
-      ]
-}]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[local]___[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'less-loader',
+          },
+        ],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: './src/index.html',
+    }),
   ],
   devServer: {
     contentBase: './dist',
-    hot: true
-  }
+    hot: true,
+  },
 };
