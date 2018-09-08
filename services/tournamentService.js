d408de994fd1a361919a8e44c8b671b78e29958e
Alfie Darko <alfiedarko@Alfies-MBP.lan>
2018-09-08 18:05:57 +0100

    Major file clean up, removed console.logs

--- services/tournamentService.js
+++ services/tournamentService.js
@@ -2,15 +2,7 @@ import { Tournament } from '../models/tournament';
 
 class TournamentService {
   static create(req) {
-    console.log('req players', req.body.players, 'req');
-    console.log('reqnew', req.body.players[0][0], 'reqnew');
-    console.log('reqnew1', req.body.players[1], 'reqnew');
-
-    let tournamentEntry = new Tournament({
-      tournamentName: req.body.tournamentName,
-      players: req.body.players,
-    });
-    console.log(tournamentEntry);
+    let tournamentEntry = new Tournament(req.body);
     return tournamentEntry.save();
   }
 }
