9f8d021221bd7701deeacc84351c1fa3fd02c2f4
Alfie Darko <alfiedarko@Alfies-MBP.lan>
2018-09-09 15:07:32 +0100

    removed commented out reducers

--- client/src/reducers/tournamentReducer.js
+++ client/src/reducers/tournamentReducer.js
@@ -23,28 +23,12 @@ export function tournamentsReducer(state = initialState.tournaments, action) {
 }
 
 // Selectors
-
 export const selectTournamentById = (state, tournamentid) => {
   return _.find(state.tournaments, { _id: tournamentid });
 };
-
-// export const selectPlayersFromTournament = createSelector(
-//   selectTournamentById(state, tournamentid),
-//   tournament => {
-//     console.log(tournament.players);
-//     tournament.players;
-//   },
-// );
 
 export const getPlayers = tournament => {
   const result = tournament.players;
   console.log('result', result);
   return result;
 };
-
-// export const selectPlayerByIdFromTournament = createSelector(
-//   selectTournamentById,
-//   (tournament, playerid) => {
-//     return tournament.players.filter(player => player.id == playerid);
-//   },
-// );
