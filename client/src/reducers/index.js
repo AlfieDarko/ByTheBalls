5c4f835ba10548615cb47378e79a6df13c44f988
Alfie Darko <alfiedarko@Alfies-MBP.lan>
2018-09-17 19:01:32 +0100

    added unused imports

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,15 +1,11 @@
 import { combineReducers } from 'redux';
-import {
-  // tournamentReducer as tournament,
-  tournamentsReducer as tournaments,
-  // tournamentPointsReducer as tournamentPoints,
-} from './tournamentReducer';
-// import players from './playersReducer';
+import { tournamentsReducer as tournaments } from './tournamentReducer';
+import playerReducer from './playerReducer';
 // import player from './playerReducer';
 
 const rootReducer = combineReducers({
   tournaments,
-  // tournaments,
+  // playerReducer,
   // tournamentPoints,
   // players,
   // player,
